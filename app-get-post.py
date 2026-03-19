7bb490f5bea8425791c3a4d26a7fa4f264be3848
cauepasilva-art <cauepasilva@gmail.com>
2026-03-19 10:39:03 -0300

    Mudei o nome

--- app-get-post.py
+++ app-get-post.py
@@ -5,7 +5,7 @@ class Servidor(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"Servidor Backend Platini Funcionando - Revisao 1")
+        self.wfile.write(b"Servidor Backend Funcionando - Revisao 1")
 
    
     def do_POST(self):
